30379d1b792a94b7f9fcf0f620be40c65d85e8e1
Amit Roy <amit.roy@emc.com>
2019-07-03 10:06:05 -0400

    wip

--- buildSrc/src/main/groovy/com/parent/TridentBuild.groovy
+++ buildSrc/src/main/groovy/com/parent/TridentBuild.groovy
@@ -8,12 +8,18 @@ import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.PathSensitive;
 import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.api.tasks.options.Option;
+import org.gradle.api.tasks.Nested;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.tasks.CacheableTask;
 import org.gradle.api.file.*;
+import java.time.LocalDateTime;
 
 @CacheableTask
 abstract class TridentBuild extends DefaultTask {
+
+    @Input
+    String forceBuild = ""
 
     @InputDirectory
     abstract DirectoryProperty getSrcDir();
@@ -21,15 +27,29 @@ abstract class TridentBuild extends DefaultTask {
     @OutputDirectory
     abstract DirectoryProperty getOutputDir(); 
 
-    @Input
-    abstract Map<String,String> getInputProperties();
-
     @TaskAction
     public void build() {
         // TODO: System.out.println("Starting build " + srcDir);
+        setForceBuild(false)
         doBuild();
     }
  
+    @Option( option = 'force', description = 'Build even if not stale in cache. Will force build only this repo')
+    void isForceBuild(final Boolean ignoreVal) {
+       setForceBuild(true)
+    }
+
+    void setForceBuild(Boolean doSet) {
+      if(doSet) {
+        forceBuild = LocalDateTime.now().toString()
+      }
+      else {
+        forceBuild = "" 
+      }
+
+    }
+   
+
     abstract void doBuild();
     
 }
